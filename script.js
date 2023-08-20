e05b37e67d3151b52bd82c564661771b0dc32881
moumitaTW <moumita@Moumitas-MacBook-Pro.local>
2023-08-20 16:52:21 +0530

    Add script.js. Handle mouseover and mouseleave

---
+++ script.js
@@ -0,0 +1,29 @@
+function handleMouseover(event) {
+    let movieCard = event.target
+    let iconSection = event.target.children[1]
+
+    iconSection.classList.remove('hidden')
+    movieCard.classList.add('glow')
+    iconSection.classList.add('glow')
+}
+
+function handleMouseLeave(event) {
+    let movieCard = event.target
+    let iconSection = event.target.children[1]
+
+    iconSection.classList.add('hidden')
+    movieCard.classList.remove('glow')
+    iconSection.classList.remove('glow')
+}
+
+
+function main() {
+    let movies = document.getElementsByClassName('movie-card');
+    for (let i = 0; i < movies.length; i++) {
+        const element = movies[i];
+        element.addEventListener('mouseover', handleMouseover)
+        element.addEventListener('mouseleave', handleMouseLeave)
+    }
+}
+
+window.onload = main
